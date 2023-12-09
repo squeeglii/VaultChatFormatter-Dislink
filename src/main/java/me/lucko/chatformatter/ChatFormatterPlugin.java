401997241732832e0cdce10079d2a902f8994fe4
Will Scully <16578982+squeeglii@users.noreply.github.com>
2023-12-09 02:33:01 +0000

    Implement dislink integration

--- src/main/java/me/lucko/chatformatter/ChatFormatterPlugin.java
+++ src/main/java/me/lucko/chatformatter/ChatFormatterPlugin.java
@@ -1,5 +1,8 @@
 package me.lucko.chatformatter;
 
+import me.squeeglii.plugin.dislink.display.VerifierPrefixes;
+import me.squeeglii.plugin.dislink.storage.LinkedAccount;
+import me.squeeglii.plugin.dislink.storage.LinkedAccountCache;
 import net.milkbowl.vault.chat.Chat;
 
 import org.bukkit.ChatColor;
@@ -13,6 +16,8 @@ import org.bukkit.event.server.ServiceRegisterEvent;
 import org.bukkit.event.server.ServiceUnregisterEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Optional;
+import java.util.UUID;
 import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,11 +33,13 @@ public class ChatFormatterPlugin extends JavaPlugin implements Listener {
     private static final String MESSAGE_PLACEHOLDER = "{message}";
     private static final String PREFIX_PLACEHOLDER = "{prefix}";
     private static final String SUFFIX_PLACEHOLDER = "{suffix}";
+    private static final String DISLINK_VERIFIER_PLACEHOLDER = "{dislink_verifier}";
 
     // Format placeholder patterns
     private static final Pattern NAME_PLACEHOLDER_PATTERN = Pattern.compile(NAME_PLACEHOLDER, Pattern.LITERAL);
     private static final Pattern PREFIX_PLACEHOLDER_PATTERN = Pattern.compile(PREFIX_PLACEHOLDER, Pattern.LITERAL);
     private static final Pattern SUFFIX_PLACEHOLDER_PATTERN = Pattern.compile(SUFFIX_PLACEHOLDER, Pattern.LITERAL);
+    private static final Pattern DISLINK_VERIFIER_PLACEHOLDER_PATTERN = Pattern.compile(DISLINK_VERIFIER_PLACEHOLDER, Pattern.LITERAL);
 
     /** The default format */
     private static final String DEFAULT_FORMAT = "<" + PREFIX_PLACEHOLDER + NAME_PLACEHOLDER + SUFFIX_PLACEHOLDER + "> " + MESSAGE_PLACEHOLDER;
@@ -49,12 +56,19 @@ public class ChatFormatterPlugin extends JavaPlugin implements Listener {
      */
     private Chat vaultChat = null;
 
+    private VerifierPrefixes verifierPrefixes = null;
+    private LinkedAccountCache linkedAccountCache = null;
+
+
     @Override
     public void onEnable() {
-        saveDefaultConfig();
-        reloadConfigValues();
-        refreshVault();
-        getServer().getPluginManager().registerEvents(this, this);
+        this.saveDefaultConfig();
+        this.reloadConfigValues();
+
+        this.refreshVault();
+        this.refreshDislink();
+
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     private void reloadConfigValues() {
@@ -66,18 +80,36 @@ public class ChatFormatterPlugin extends JavaPlugin implements Listener {
     }
 
     private void refreshVault() {
-        Chat vaultChat = getServer().getServicesManager().load(Chat.class);
-        if (vaultChat != this.vaultChat) {
-            getLogger().info("New Vault Chat implementation registered: " + (vaultChat == null ? "null" : vaultChat.getName()));
+        Chat newImpl = getServer().getServicesManager().load(Chat.class);
+
+        if (newImpl != this.vaultChat) {
+            this.getLogger().info("New Vault Chat implementation registered: " + (newImpl == null ? "null" : newImpl.getName()));
         }
-        this.vaultChat = vaultChat;
+
+        this.vaultChat = newImpl;
+    }
+
+    private void refreshDislink() {
+        LinkedAccountCache accountCache = getServer().getServicesManager().load(LinkedAccountCache.class);
+        VerifierPrefixes prefixRegistry = getServer().getServicesManager().load(VerifierPrefixes.class);
+
+        if (accountCache != this.linkedAccountCache) {
+            this.getLogger().info("Linked Account Cache source changed!");
+        }
+
+        if (prefixRegistry != this.verifierPrefixes) {
+            this.getLogger().info("Verifier Prefix Registry source changed!");
+        }
+
+        this.linkedAccountCache = accountCache;
+        this.verifierPrefixes = prefixRegistry;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length != 0 && args[0].equalsIgnoreCase("reload")) {
-            reloadConfig();
-            reloadConfigValues();
+            this.reloadConfig();
+            this.reloadConfigValues();
 
             sender.sendMessage("Reloaded successfully.");
             return true;
@@ -88,16 +120,26 @@ public class ChatFormatterPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onServiceChange(ServiceRegisterEvent e) {
-        if (e.getProvider().getService() == Chat.class) {
-            refreshVault();
-        }
+        if (e.getProvider().getService() == Chat.class)
+            this.refreshVault();
+
+        boolean hasDislinkUpdated = e.getProvider().getService() == LinkedAccountCache.class ||
+                                    e.getProvider().getService() == VerifierPrefixes.class;
+
+        if(hasDislinkUpdated)
+            this.refreshDislink();
     }
 
     @EventHandler
     public void onServiceChange(ServiceUnregisterEvent e) {
-        if (e.getProvider().getService() == Chat.class) {
-            refreshVault();
-        }
+        if (e.getProvider().getService() == Chat.class)
+            this.refreshVault();
+
+        boolean hasDislinkUpdated = e.getProvider().getService() == LinkedAccountCache.class ||
+                                    e.getProvider().getService() == VerifierPrefixes.class;
+
+        if(hasDislinkUpdated)
+            this.refreshDislink();
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -115,9 +157,36 @@ public class ChatFormatterPlugin extends JavaPlugin implements Listener {
             format = replaceAll(PREFIX_PLACEHOLDER_PATTERN, format, () -> colorize(this.vaultChat.getPlayerPrefix(e.getPlayer())));
             format = replaceAll(SUFFIX_PLACEHOLDER_PATTERN, format, () -> colorize(this.vaultChat.getPlayerSuffix(e.getPlayer())));
         }
+
+        processDislinkFormatting(format, e.getPlayer().getUniqueId());
+
+
+
         format = replaceAll(NAME_PLACEHOLDER_PATTERN, format, () -> e.getPlayer().getName());
 
         e.setFormat(format);
+    }
+
+    private String processDislinkFormatting(String format, UUID playerId) {
+        if(this.verifierPrefixes == null) return format;
+        if(this.linkedAccountCache == null) return format;
+
+        Optional<LinkedAccount> optLink = this.linkedAccountCache.getAccount(playerId);
+
+        if(optLink.isEmpty())
+            return format;
+
+        LinkedAccount account = optLink.get();
+        String verifier = account.verifier();
+        String verifierPrefix = this.verifierPrefixes.getPrefixForVerifier(verifier).orElse("");
+
+        if(verifierPrefix.isEmpty())
+            return format;
+
+        return replaceAll(
+                DISLINK_VERIFIER_PLACEHOLDER_PATTERN, format,
+                () -> colorize(verifierPrefix) + " "
+        );
     }
 
     /**
